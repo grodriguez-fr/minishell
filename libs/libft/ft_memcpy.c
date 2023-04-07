4ddcc04a30a10359e0f6c15c4fc5b73eca7d0a91
zorkz <alexandre.stachnik2@gmail.com>
2023-04-07 21:24:29 +0200

    debut parsing

--- libs/libft/ft_memcpy.c
+++ libs/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:10:43 by astachni          #+#    #+#             */
-/*   Updated: 2023/04/07 14:52:03 by astachni         ###   ########.fr       */
+/*   Updated: 2023/04/07 21:23:03 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
 		i++;
 	}
+	*(unsigned char *)(dest + i) = 0;
 	return (dest);
 }
--- src/parsing/parse_command.c
+++ src/parsing/parse_command.c
@@ -6,11 +6,14 @@
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 18:24:16 by astachni          #+#    #+#             */
-/*   Updated: 2023/04/07 20:24:25 by astachni         ###   ########.fr       */
+/*   Updated: 2023/04/07 21:19:48 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
+
+int		skip_alloc(char *input, t_exec **exec);
+void	fill_tab(char *input, t_exec **exec, int i);
 
 void	parse_and_exec(char *input, t_mini mini)
 {
@@ -22,6 +25,11 @@ void	parse_and_exec(char *input, t_mini mini)
 	}
 	(&mini)->ex = malloc(sizeof(t_exec));
 	parse_cmd(input, &mini.ex);
+	int	i = 0;
+	ft_printf("%s ", mini.ex->cmd_name);
+	while (mini.ex->args[i])
+		ft_printf("%s ", mini.ex->args[i++]);
+	ft_printf("\n");
 }
 
 void	parse_cmd(char *input, t_exec **exec)
@@ -30,18 +38,8 @@ void	parse_cmd(char *input, t_exec **exec)
 	int		save_i;
 	int		arg_number;
 	int		cmd_size;
-	char	*cmd;
 
-	i = 0;
-	cmd = NULL;
-	while (input && input[i] && input[i] == ' ')
-		i++;
-	cmd_size = 0;
-	while (input && input[i + cmd_size] && input[i + cmd_size] != ' ')
-		cmd_size++;
-	cmd = malloc(sizeof(char) * (cmd_size + 1));
-	ft_memcpy(cmd, &input[i], cmd_size);
-	i += cmd_size;
+	i += skip_alloc(input, exec);
 	arg_number = 0;
 	save_i = i;
 	while (input && input[i] && (input[i] != '|'
@@ -58,6 +56,32 @@ void	parse_cmd(char *input, t_exec **exec)
 	}
 	i = save_i;
 	(*exec)->args = malloc(sizeof(char *) * (arg_number + 1));
+	fill_tab(input, exec, i);
+}
+
+int	skip_alloc(char *input, t_exec **exec)
+{
+	int	i;
+	int	cmd_size;
+
+	i = 0;
+	(*exec)->cmd_name = NULL;
+	while (input && input[i] && input[i] == ' ')
+		i++;
+	cmd_size = 0;
+	while (input && input[i + cmd_size] && input[i + cmd_size] != ' ')
+		cmd_size++;
+	(*exec)->cmd_name = malloc(sizeof(char) * (cmd_size + 1));
+	ft_memcpy((*exec)->cmd_name, &input[i], cmd_size);
+	return (i + cmd_size);
+}
+
+void	fill_tab(char *input, t_exec **exec, int i)
+{
+	int	arg_number;
+	int	save_i;
+	int	cmd_size;
+
 	arg_number = 0;
 	while (input && input[i] && (input[i] != '|'
 			|| ft_memcmp(&input[i], ">>", 2) == 0))
@@ -75,9 +99,7 @@ void	parse_cmd(char *input, t_exec **exec)
 		i = save_i;
 		ft_memcpy((*exec)->args[arg_number], &input[i], cmd_size);
 		while (input[i] && input[i] != ' ')
-		{
 			i++;
-		}
 		arg_number++;
 	}
 	(*exec)->args[arg_number] = NULL;
