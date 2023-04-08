1111afb78f1fcb26332c7691cfe0021a3769e5c7
zorkz <alexandre.stachnik2@gmail.com>
2023-04-08 17:14:20 +0200

    parsing avec pipe

--- src/parsing/parse_command.c
+++ src/parsing/parse_command.c
@@ -6,13 +6,13 @@
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 18:24:16 by astachni          #+#    #+#             */
-/*   Updated: 2023/04/08 16:30:50 by astachni         ###   ########.fr       */
+/*   Updated: 2023/04/08 17:13:34 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-t_exec	*parse_cmd_args(int i, char **commands, char *cmd_name, t_exec *exec);
+t_exec	*parse_cmd_args(int i, char **comm, char *cmd_name, t_exec *exec);
 
 void	parse_and_exec(char *input, t_mini mini)
 {
@@ -41,28 +41,28 @@ t_exec	*parse_cmd(char *input, t_exec *exec)
 	}
 }
 
-t_exec	*parse_cmd_args(int i, char **commands, char *cmd_name, t_exec *exec)
+t_exec	*parse_cmd_args(int i, char **comm, char *cmd_name, t_exec *exec)
 {
 	int		j;
 	int		save_j;
 	char	**args;
 
 	j = 0;
-	while (commands && commands[i] && commands[i][j])
+	while (comm && comm[i] && comm[i][j])
 	{
-		while (commands[i] && commands[i][j] && commands[i][j] == ' ')
+		cmd_name = NULL;
+		while (comm[i] && comm[i][j] && comm[i][j] == ' ')
 			j++;
 		save_j = j;
-		while (commands[i] && commands[i][j + save_j] &&
-			commands[i][j + save_j] != ' ')
+		while (comm[i] && comm[i][j + save_j] && comm[i][j + save_j] != ' ')
 			save_j++;
-		cmd_name = ft_calloc(sizeof(char), save_j + 1);
-		ft_memcpy(cmd_name, &commands[i][j], save_j);
+		cmd_name = malloc(sizeof(char) * (save_j + 1));
+		ft_memcpy(cmd_name, &comm[i][j], save_j);
 		j += save_j;
-		while (commands[i] && commands[i][j] && commands[i][j] == ' ')
+		while (comm[i] && comm[i][j] && comm[i][j] == ' ')
 			j++;
-		args = ft_split(&commands[i][j], ' ');
-		while (commands[i][j])
+		args = ft_split(&comm[i][j], ' ');
+		while (comm[i][j])
 			j++;
 		add_cmd(&exec, cmd_name, args);
 	}
