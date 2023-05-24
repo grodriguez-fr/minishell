4483292fe8aa2d5e278842a1b0c6b17b774d9777
Alexandre Stachnik <astachni@z1r7p2.42lyon.fr>
2023-05-24 17:21:32 +0200

    les gillemets ptn

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: astachni <astachni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 14:21:35 by astachni          #+#    #+#             */
-/*   Updated: 2023/05/13 18:20:53 by astachni         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:31:53 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 int	main(int ac, char **av, char **envp)
 {
-	t_mini	mini;
+	// struct sigaction	sa;
+	// sigset_t			mask;
+	t_mini				mini;
 
 	(void)ac;
 	(void)av;
 	mini.env = NULL;
 	mini.ex = NULL;
 	mini.env_start = envp;
+	// sigemptyset(&mask);
+	// sigaddset(&mask, SIGINT);
+	// sigaddset(&mask, SIGQUIT);
+	// sa.sa_handler = 0;
+	// sa.sa_flags = SA_SIGINFO;
+	// sa.sa_mask = mask;
+	// sa.sa_sigaction = signal_handler;
 	parse_env(envp, &mini.env, mini);
-	signal(SIGINT, signal_handler);
+	// sigaction(SIGINT, &sa, (void *)mini.env);
 	launch_minishell_img();
 	prompt(mini);
 	return (0);
