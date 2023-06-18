d1e6797948d18df7df93fa6ff8fe29dc6f404ab7
ZorkZ <zorkz@pop-os.localdomain>
2023-06-18 15:35:52 +0200

    fion

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -1,61 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 15:33:16 by astachni          #+#    #+#             */
+/*   Updated: 2023/06/18 15:33:20 by astachni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void replace_var(t_mini *mini, char **to_replace)
+void	replace_var(t_mini *mini, char **to_replace)
 {
-    t_env_p *env;
+	t_env_p	*env;
 
-    env = mini->env;
-    while (env)
-    {
-        if (same_string(env->key, &(*to_replace)[1]))
-        {
-            free(*to_replace);
-            *to_replace = ft_strdup(env->value);
-            return ;
-        }
-        env = env->next;
-    }
-    if ((*to_replace)[1] == '?' && !(*to_replace)[2])
-    {
-        free(*to_replace);
-        *to_replace = ft_itoa(mini->command_ret);
-        return ;
-    }
-    free(*to_replace);
-    *to_replace = malloc(sizeof(char));
-    (*to_replace)[0] = 0;
+	env = mini->env;
+	while (env)
+	{
+		if (same_string(env->key, &(*to_replace)[1]))
+		{
+			free(*to_replace);
+			*to_replace = ft_strdup(env->value);
+			return ;
+		}
+		env = env->next;
+	}
+	if ((*to_replace)[1] == '?' && !(*to_replace)[2])
+	{
+		free(*to_replace);
+		*to_replace = ft_itoa(mini->command_ret);
+		return ;
+	}
+	free(*to_replace);
+	*to_replace = malloc(sizeof(char));
+	(*to_replace)[0] = 0;
 }
 
-int ap_before(char *comm, int i)
+int	ap_before(char *comm, int i)
 {
-    int j;
+	int	j;
 
-    j = i;
-    while (j >= 0)
-        if (comm[j--] == '\'')
-            return (1);
-    return (0);
+	j = i;
+	while (j >= 0)
+		if (comm[j--] == '\'')
+			return (1);
+	return (0);
 }
 
-int ap_after(char *comm, int i)
+int	ap_after(char *comm, int i)
 {
-    int j;
+	int	j;
 
-    j = i;
-    while (comm[j])
-        if (comm[j++] == '\'')
-            return (1);
-    return (0); 
+	j = i;
+	while (comm[j])
+		if (comm[j++] == '\'')
+			return (1);
+	return (0);
 }
 
-int should_expand(char *arg, char *comm)
+int	should_expand(char *arg, char *comm)
 {
-    int found;
+	int	found;
 
-    found = ft_strstr(comm, arg);
-    if (found == -1)
-        return (0);
-    return (!ap_before(comm, found) || !ap_after(comm, found));
+	found = ft_strstr(comm, arg);
+	if (found == -1)
+		return (0);
+	return (!ap_before(comm, found) || !ap_after(comm, found));
 }
 
 char	**take_var(t_mini *mini, char *comm, char **args)
@@ -67,8 +79,9 @@ char	**take_var(t_mini *mini, char *comm, char **args)
 	i = 0;
 	while (args && args[i])
 	{
-		if (ft_strncmp(args[i], "$", 1) == 0 && should_expand(args[i], comm) && args[i][1])
-            replace_var(mini, &args[i]);
+		if (ft_strncmp(args[i], "$", 1) == 0
+			&& should_expand(args[i], comm) && args[i][1])
+			replace_var(mini, &args[i]);
 		i++;
 	}
 	return (args);
